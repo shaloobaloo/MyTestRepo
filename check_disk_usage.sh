a94bebbfd528259e173a98e6decf2f87a2a2fa13
shaloobaloo <pystud08@thecalc.site>
2022-08-22 11:32:22 +0300

    change

---
+++ check_disk_usage.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+dir="$1"
+if [ -d $dir ]; then
+    echo -e "Checking disk usage in $dir\n"
+else
+    echo -e "$dir is not a directory!\n"
+    exit 101
+fi
+echo "The heaviest subfolder of $dir is:"
+top1=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 2 | \
+	 tail -n 1| cut -f 2)
+disk=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 2 | \
+	 tail -n 1| cut -f 1)
+echo -e "$top1 with disk usage of $disk"
